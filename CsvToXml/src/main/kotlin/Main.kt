db327c77f36655625a887aea15fd22f83ce29a73
CurroCobain <123578572+CurroCobain@users.noreply.github.com>
2023-09-26 20:37:04 +0200

    Add files via upload

--- CsvToXml/src/main/kotlin/Main.kt
+++ CsvToXml/src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 import java.io.File
+import org.w3c.dom.Document
+import javax.xml.parsers.DocumentBuilderFactory
+
 fun main() {
     /*
     Primero vamos a crear las distintas variables y objetos que vamos a necesitar para la transformación a XML
@@ -13,37 +16,40 @@ fun main() {
     var con = 0 // Esta variable nos ayudará en la escritura del archivo xml
     var listaCampos = mutableListOf<String>()
     // Esta variable es una lista formada por los distintos elementos y atributos que la obtendremos más adelante
-    var textoXml="<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <FUENTES> \n " //creamos una variable String con el encabezamiento del archivo xml
+    var textoXml="<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n <fuentes> \n " //creamos una variable String con el encabezamiento del archivo xml
     for (linea in buff.readLines()) {
         var listaValores = mutableListOf<String>()// Lista que usaremos para ir añadiendo el contenido de las distintas etiquetas del xml
         if (con < 1) {
-            for (i in linea.split(";")) {
+            for (i in linea.replace("\uFEFF","").split(";")) {
                 listaCampos.add(i) //obtenemos los campos y atributos de la primera fila del archivo csv
                 con++
             }
         } else if(con >= 1) {
             for (i in linea.split(";")) { // de cada linea obtenemos el contenido de las etiquetas
+                i.replace("\"","")
                 listaValores.add(i)
             }
         }
         for (x in listaCampos.indices){
             for(j in listaValores.indices){
                 if(x == 0){
-                    textoXml+="          "+"<FUENTE> \n" +"                    "+"<${listaCampos[0]}> ${listaValores[0]} </${listaCampos[0]}> \n"
+                    textoXml+="     <fuente> \n"
+                    textoXml+="         <${listaCampos[0].toLowerCase()}>${listaValores[0].toLowerCase()}</${listaCampos[0].toLowerCase()}> \n".replace("\"","")
                     break
                 }
                 else if(x == j){
-                    textoXml+="                    "+"<${listaCampos[x]}> ${listaValores[j]} </${listaCampos[x]}> \n"
+                    textoXml+="         <${listaCampos[x].toLowerCase()}>${listaValores[j].toLowerCase()}</${listaCampos[x].toLowerCase()}> \n".replace("\"","")
                 }
                 else if(x == listaCampos.size-1){
-                    textoXml+="           </FUENTE>\n "
+                    textoXml+="     </fuente>\n "
                     break
                 }
             }
         }//Con este bucle obtenemos y añadimos al texto los distintos campos y valores del archivo csv y los añadimos al texto que escribiremos en el xml
     }
-    textoXml+="</FUENTES> " // Cerramos el texto con la etiqueta correspondiente
+    textoXml+="</fuentes>" // Cerramos el texto con la etiqueta correspondiente
     newXml.writeText(textoXml) // sobreescribimos el archivo con el texto obtenido del archivo csv
+
 }
 
 
